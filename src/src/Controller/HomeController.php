e3ff3629f2782b846a66d02697ca8cbc7a27fbc5
HassanAriz <40475312+HassanAriz@users.noreply.github.com>
2019-02-15 11:56:08 +0100

    Front/homepage (#11)
    * add homecontroller
    
    * navbar
    
    * add conferences
    
    * add conferences view
    
    * add conferences/navbar front-end
    
    * add Home Controller
    
    * gitignore

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -8,12 +8,64 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/homepage", name="home")
      */
     public function index()
     {
+        $conferences =[
+            0 => [
+                'title' => 'Bonjour',
+                'rating' => 1,
+                'address' => '1 Avenue Charles de Gaulle 93270 Sevran',
+                'users' => 'le chef',
+            ],
+            1 => [
+                'title' => 'Salut',
+                'rating' => 3,
+                'address' => '26 Rue de la mort 97125 Bouilantes',
+                'users' => 'le roi',
+            ],
+            2 => [
+                'title' => 'Hello',
+                'rating' => 5,
+                'address' => '10 Chemin du Marais du Soucis 93270 Sevran',
+                'users' => 'le chef',
+            ],
+        ];
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'conferences' => $conferences,
         ]);
     }
+
+    /*/**
+    * @Route("/registration", name="home")
+    */
+   /* public function Registration()
+    {
+        $conferences =[
+            0 => [
+                'title' => 'Bonjour',
+                'rating' => 1,
+                'address' => '1 Avenue Charles de Gaulle 93270 Sevran',
+                'users' => 'le chef',
+            ],
+            1 => [
+                'title' => 'Salut',
+                'rating' => 3,
+                'address' => '26 Rue de la mort 97125 Bouilantes',
+                'users' => 'le roi',
+            ],
+            2 => [
+                'title' => 'Hello',
+                'rating' => 5,
+                'address' => '10 Chemin du Marais du Soucis 93270 Sevran',
+                'users' => 'le chef',
+            ],
+        ];
+
+        return $this->render('home/index.html.twig', [
+            'conferences' => $conferences,
+        ]);
+    }*/
 }
