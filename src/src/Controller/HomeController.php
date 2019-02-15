e6fc91d7cee026bded25dcc2dac70cbd4e631a6e
Hassan Ariz <>
2019-02-15 11:42:47 +0100

    add Home Controller

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -37,4 +37,35 @@ class HomeController extends AbstractController
             'conferences' => $conferences,
         ]);
     }
+
+    /*/**
+    * @Route("/registration", name="home")
+    */
+   /* public function Registration()
+    {
+        $conferences =[
+            0 => [
+                'title' => 'Bonjour',
+                'rating' => 1,
+                'address' => '1 Avenue Charles de Gaulle 93270 Sevran',
+                'users' => 'le chef',
+            ],
+            1 => [
+                'title' => 'Salut',
+                'rating' => 3,
+                'address' => '26 Rue de la mort 97125 Bouilantes',
+                'users' => 'le roi',
+            ],
+            2 => [
+                'title' => 'Hello',
+                'rating' => 5,
+                'address' => '10 Chemin du Marais du Soucis 93270 Sevran',
+                'users' => 'le chef',
+            ],
+        ];
+
+        return $this->render('home/index.html.twig', [
+            'conferences' => $conferences,
+        ]);
+    }*/
 }
